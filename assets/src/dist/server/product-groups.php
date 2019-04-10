725dbdd5fd0b9f3fa5f9009bc0c986cd8f0a9d3e
MaksimLavrenyuk <maksim.lavrenyuk@outlook.com>
2019-04-10 11:30:21 +0400

    added delete product-gorup

--- assets/src/dist/server/product-groups.php
+++ assets/src/dist/server/product-groups.php
@@ -2,12 +2,15 @@
     header("Content-Type: text/html; charset=utf-8");
     mb_internal_encoding('UTF-8');
 
-    $msg = 'Добавлен';
+    //$msg = 'Добавлен';
+    $msg = 'Удален';
     $productGroupName = '3БФ24';
+    $productGroupID = 5;
 
 
     $result['msg'] = $msg;
     $result['productGroupName'] = $productGroupName;
+    $result['productGroupID'] = $productGroupID;
 
    echo json_encode($result, JSON_UNESCAPED_UNICODE);
 ?>
